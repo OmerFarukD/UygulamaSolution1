80e03ce3b3f4ad314de2099b9e1ccd7765482b3d
omerc <omerc@DESKTOP-P7FDVCL>
2023-08-18 18:59:00 +0300

    Day 2 Commit

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -61,32 +61,158 @@ else if(vizeNotu >55)
 
 // HAFTANIN 1. GÜNÜ PAZARTESİ 2. GÜN SALI ... 
 
-Console.WriteLine("Haftanın kaçıncı gününün adını öğrenmek istiyorsunuz :");
-int day = Convert.ToInt32(Console.ReadLine());
-if (day <= 5 && day >= 1)
+//Console.WriteLine("Haftanın kaçıncı gününün adını öğrenmek istiyorsunuz :");
+//int day = Convert.ToInt32(Console.ReadLine());
+//if (day <= 5 && day >= 1)
+//{
+//    Console.WriteLine("Hafta içi-");
+//}
+//else if (day>=6 && day <=7)
+//{
+//    Console.WriteLine("Hafta sonu-");
+//}
+//if (day==1)
+//{
+//    Console.WriteLine("Günlerden pazartesidir.");
+//}
+//else if (day == 2)
+//{
+//    Console.WriteLine("Günlerden salıdır.");
+//}
+//else if ( day ==6)
+//{
+//    Console.WriteLine("Günlerden Cumartesi");
+//}
+//else if (day == 7)
+//{
+//    Console.WriteLine("Günlerden Pazar");
+//}
+
+//Console.WriteLine("1. Sayıyı giriniz.");
+//int sayi1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("2 . Sayıyı giriniz");
+//int sayi2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("İşlem Seçiniz: \n +- Toplama \n (-)-Çıkarma" +
+//    "\n (*)-Çarpma " +
+//    "\n (/)-Bölme");
+
+//string @operator = Console.ReadLine();
+// if else ile kullanımı
+//if (@operator.Equals("+"))
+//    Console.WriteLine($"2 sayının toplamı = {sayi1 + sayi2}");
+//else if (@operator.Equals("-"))
+//    Console.WriteLine($"2 sayının farkı = {sayi1 - sayi2}");
+//else if (@operator == "*")
+//    Console.WriteLine($"2 Sayını çarpımı = {sayi1 * sayi2}");
+//else if (@operator == "/")
+//    Console.WriteLine($"2 Sayının Bölümü = {sayi1 / sayi2}");
+//else Console.WriteLine("Geçersiz karakter girdiniz.");
+
+//switch (@operator)
+//{
+//    case "+":
+//        Console.WriteLine($"2 sayının toplamı = {sayi1 + sayi2}");
+//        break;
+//    case "-":
+//        Console.WriteLine($"2 sayının farkı = {sayi1 - sayi2}");
+//        break;
+//    case "*":
+//        Console.WriteLine($"2 Sayını çarpımı = {sayi1 * sayi2}");
+//        break;
+//    case "/":
+//        {
+//            if(sayi2 ==0)
+//            {
+//                Console.WriteLine("2. Sayı 0 olamaz");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"2 Sayının Bölümü = {sayi1 / sayi2}");
+            
+//            }
+//            break;
+//        }
+       
+
+//    default:
+//        Console.WriteLine("Geçersiz karakter girdiniz");
+//        break;
+//}
+
+// Cololections 
+int[] sayilar = {1,2,3,4,6,10,11,12,2,5,6};
+Console.WriteLine($"3. Sayı {sayilar[2]}");
+Console.WriteLine($"Dizinin Boyutu : {sayilar.Length}");
+Console.WriteLine($"Dizinin en büyük elemanı : {sayilar.Max()}");
+Console.WriteLine($"Dizinin en küçük elemanı : {sayilar.Min()}");
+Console.WriteLine($"Dizinin tipi nedir : {sayilar.GetType()}");
+
+List<int> sayilarListesi = new List<int>() {1,2,3};
+sayilarListesi.Add(2);
+sayilarListesi.Remove(3);
+Console.WriteLine($"sayılar Listesinin Boyutu : {sayilarListesi.Count}");
+// Generic programing
+List<string> sehirler = new();
+sehirler.Add("Elazığ");
+sehirler.Add("Malatya");
+sehirler.Add("Bingöl");
+sehirler.Add("Tunceli");
+Console.WriteLine($"Şehirler listesinin Boyutu : {sehirler.Count}");
+Console.WriteLine(sehirler);
+
+
+// for döngüsü yöntemi ile 
+for (int i =0; i<sehirler.Count;i++)
 {
-    Console.WriteLine("Hafta içi-");
+    Console.WriteLine($"{i+1}. Şehir = {sehirler[i]}");
 }
-else if (day>=6 && day <=7)
+
+//foreach
+foreach (string sehir in sehirler) { 
+    Console.WriteLine(sehir);
+}
+
+// Meyve Listesi tutan bir Liste oluşturun ve ekrana bütün meyveleri getiren foreach döngüsünü kullanın
+// bir sınav notu listesi olsun ve bu sınav notu listesinin for döngüsü ile ekrana yazdırın.
+
+List<string> meyveler = new() { "Elma","Üzüm","Karpuz"};
+foreach (string item in meyveler)
 {
-    Console.WriteLine("Hafta sonu-");
+    Console.WriteLine(item);
 }
-if (day==1)
+
+List<int> notes = new List<int> { 10,20,30};
+for (int i= 0;i<notes.Count;i++)
 {
-    Console.WriteLine("Günlerden pazartesidir.");
+    Console.WriteLine(notes[i]);
 }
-else if (day == 2)
-{
-    Console.WriteLine("Günlerden salıdır.");
-}
-else if ( day ==6)
-{
-    Console.WriteLine("Günlerden Cumartesi");
-}
-else if (day == 7)
-{
-    Console.WriteLine("Günlerden Pazar");
-}
+Console.WriteLine("2. Yöntem");
+meyveler.ForEach(item=>Console.WriteLine(item));
+// LINQ Language Integrated Query
+
+// Elimizde 1 tane liste olsun Bu liste de çift sayı olanları farklı bir listeye atsın ve o çift sayı listesini ekrana bastırsın
+List<int> numbers = new() {1,2,3,4,5,6,7,8,9,10};
+List<int> evenNumbers = new();
+List<int> oddNumbers = new();
+//foreach (int number in numbers)
+//{
+//    if (number % 2==0)
+//    {
+//        evenNumbers.Add(number);
+//    }
+//    else
+//    {
+//      oddNumbers.Add(number);
+//    }
+//}
+
+oddNumbers = numbers.FindAll(odd=> odd%2!=0);
+evenNumbers = numbers.FindAll(e=> e%2==0);
+numbers.FindAll(odd => odd % 2 != 0).ForEach(x=> Console.WriteLine(x));
+Console.WriteLine("Çift Sayılar");
+evenNumbers.ForEach(even => Console.WriteLine(even));
+Console.WriteLine("Tek sayılar");
+oddNumbers.ForEach(x => Console.WriteLine(x));
 
 
 
