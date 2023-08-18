64eca7a12af92b9ceb79f77055cfc0fc5c2aef0a
omerc <omerc@DESKTOP-P7FDVCL>
2023-08-18 11:07:38 +0300

    Add project files.

---
+++ Day2/Program.cs
@@ -0,0 +1,94 @@
+﻿// Ve veya 
+//  & -> Ve 
+// | -> Veya 
+Console.WriteLine(1|1);
+Console.WriteLine(0|1);
+Console.WriteLine(true | true);
+Console.WriteLine(0 & 1);
+
+// if - else 
+
+
+// == -> eşit mi 
+// != -> eşit değil mi 
+int sayi = 20;
+
+if (sayi > 20)
+{
+    Console.WriteLine("sayı 20 den büyüktür.");
+}
+else if (sayi ==20)
+{
+Console.WriteLine("Sayı 20 ye eşittir.");
+}
+
+else
+{
+    Console.WriteLine("Sayı 20 den küçüktür.");
+}
+
+// vize notu 55 den küçükse kaldınız
+// 55 ten büyükse geçtiniz
+//100 ise tam puan aldınız.
+int vizeNotu = 100;
+if (vizeNotu < 55)
+{
+    Console.WriteLine("Kaldınız.");
+}
+else if(vizeNotu ==100)
+{
+    Console.WriteLine("Tam puan aldınız.");
+}
+else if(vizeNotu >55)
+{
+    Console.WriteLine("Geçtiniz.");
+}
+
+// kullanıcıdan yaşını alsın ve ehliyet alabilr mi alabiliyorsa ehliyet alabilir
+//alamazsa ehliyet alamaz.
+
+//Console.WriteLine("Lütfen yaşınızı girin : ");
+//int age = Convert.ToInt32(Console.ReadLine());
+
+//if(age >= 18)
+//{
+//    Console.WriteLine("Ehliyet alabilir");
+//}
+//else
+//{
+//    Console.WriteLine("Ehliyet alamaz.");
+//}
+
+// HAFTANIN 1. GÜNÜ PAZARTESİ 2. GÜN SALI ... 
+
+Console.WriteLine("Haftanın kaçıncı gününün adını öğrenmek istiyorsunuz :");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day <= 5 && day >= 1)
+{
+    Console.WriteLine("Hafta içi-");
+}
+else if (day>=6 && day <=7)
+{
+    Console.WriteLine("Hafta sonu-");
+}
+if (day==1)
+{
+    Console.WriteLine("Günlerden pazartesidir.");
+}
+else if (day == 2)
+{
+    Console.WriteLine("Günlerden salıdır.");
+}
+else if ( day ==6)
+{
+    Console.WriteLine("Günlerden Cumartesi");
+}
+else if (day == 7)
+{
+    Console.WriteLine("Günlerden Pazar");
+}
+
+
+
+
+
---
+++ FirstExample/Program.cs
@@ -0,0 +1,81 @@
+﻿// Değer tipler , Referans Tipler 
+// Değer tipler : bool (Doğruluk değeri tutan veri tipidir.)
+// int : sayısal değerler tutar.
+// Float veya Double : Ondalıklı sayı tutar.
+// Sayısal Değerler
+//int sayi1 = 10;
+//Console.WriteLine(sayi1);
+
+//bool
+//bool dogruluk = false;
+//bool dogruluk1= true;
+//Console.WriteLine(dogruluk1);
+//Console.WriteLine(dogruluk);
+// Değer Tiplerin Özellikleri
+// Hafızanın stack kısmında tutulur.
+// null değer alamazlar.
+//int? sayi2=null;
+//Console.WriteLine(sayi2);
+// Referans Tipler 
+// Hafızanın Heap kısmında tutulur
+// string, Array,HashMap,List,class, interface 
+
+
+//string metin = " İlk metin örneği";
+//Console.WriteLine(metin);
+//todo: iki tane dğişken olsun ve bu 2 değişkeni toplasın ekrana bastırsın. 
+int a= 10;
+int b= 15;
+int toplam = a + b;
+// 1.Senaryo
+Console.WriteLine(toplam);
+// 2. Senaryo 
+Console.WriteLine("iki sayının toplamı = " + a + "+" + b + "= " + toplam);
+// String interpolation
+Console.WriteLine($"iki sayının toplamı = {a} + {b} = {toplam}");
+string metin1 = "metin1"; //metin1metin2
+string metin2 = "metin2";
+string birlestirilmisMetin = metin1 + metin2;
+Console.WriteLine(birlestirilmisMetin);
+Console.WriteLine("İki sayının toplamı" + a + b);
+Console.WriteLine(a + b);
+
+
+char aa = 'A';
+Console.WriteLine((int)aa);
+
+string example = "Küçük Büyük veya String örneği";
+// Stringin içindei her karakteri büyük harf yapar.
+Console.WriteLine(example.ToUpper());
+//Stringin içindei her karakteri küçük harf yapar.
+Console.WriteLine(example.ToLower());
+// K harfi ile başlıyor mu
+Console.WriteLine(example.StartsWith("k"));
+
+// O harfi ile bitiyormu
+Console.WriteLine(example.EndsWith("O"));
+
+// metnin ilk 4 harfini ekrana bastırsın.
+Console.WriteLine(example[..4]);
+
+// metinin ilk 4 hanesinden swonraki bütün karakterleri getir
+Console.WriteLine(example[3..]);
+Console.WriteLine(example.GetType());
+
+// kod satırları sırasıyla toplama çıkarma bölme ve çarpma işlemleri yapsın sonuçolarak
+// iki sayının çarpımı ....
+// kilo/ boy*boy
+
+// Dairenin Çevresini ve alanını hesaplayan kodları yazınız
+// alan : pi * r^2
+// çevre : 2*pi*r
+
+double yaricap = 10.0;
+double pi = Math.PI;
+double alan = pi * Math.Pow(yaricap,2);
+double cevre = 2 * pi * yaricap;
+Console.WriteLine($" yarıçapı : \n {yaricap} olan dairenin alanı : {alan}");
+Console.WriteLine("çevresi : " + cevre);
+
+
+
